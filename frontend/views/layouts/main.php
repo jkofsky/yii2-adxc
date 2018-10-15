280ecd48c935e9681872e2663461c6fc6b94d3b1
jkofsky <jkofsky@ADX-TOC>
2018-10-15 17:35:48 -0500

    Podcast page update

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -2,7 +2,7 @@
 /* @var $this \yii\web\View */
 /* @var $content string */
 
-use yii\helpers\Html;
+use yii\bootstrap\Html;
 use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
 use yii\widgets\Breadcrumbs;
