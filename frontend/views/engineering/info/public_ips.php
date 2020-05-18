408ec3c03b46a864d1bdee61408df52394d1f800
jkofsky <jkofsky@ADX-TOC>
2020-05-18 17:25:14 -0500

    Modified Public IPs

--- frontend/views/engineering/info/public_ips.php
+++ frontend/views/engineering/info/public_ips.php
@@ -14,10 +14,10 @@ $this->params['breadcrumbs'][] = 'Public IP Addresses';
 <section class="row">
     <div class="col-md-4">
         <div class="panel panel-info">
-            <div class="panel-heading"><?= Html::encode('AT&T Fiber (AudioNet)'); ?></div>
+            <div class="panel-heading"><?= Html::encode('AT&T In TOC (AudioNet)'); ?></div>
             <div class="panel-body">
                 <address>
-                    <b>Modem s/n: </b>
+                    <b>Modem s/n: </b><u>Fiber</u>
                 </address>
                 <pre>
 255.255.255.240 | Netmask
@@ -26,87 +26,36 @@ $this->params['breadcrumbs'][] = 'Public IP Addresses';
  12. 41.153.193 | Cisco Eth 0/0
  12. 41.153.194 | SonicWall (Audio)
                 | <i>(pfSense)</i>
- 12. 41.153.195 | 
- 12. 41.153.196 | UWF BricLink
- 12. 41.153.197 | WEBY BricLink
- 12. 41.153.198 | opal1.adxc.com
- 12. 41.153.199 | opal2.adxc.com
- 12. 41.153.200 | 
- 12. 41.153.201 | 
- 12. 41.153.202 | 
- 12. 41.153.203 | Internet Streaming
- 12. 41.153.204 | Remote Comrex #1
- 12. 41.153.205 | Comrex @ 192.168.12.6<br>
-                | <i>Remote Comrex #2</i>
+ 12. 41.153.195 | opal1.adxc.com (WYCT)
+ 12. 41.153.196 | opal2.adxc.com (WNRP)
+ 12. 41.153.197 | opal3.adxc.com (WEBY/ESPN)
+ 12. 41.153.198 | opal4.adxc.com (Studio 3)
+ 12. 41.153.199 | BricLink II
+ 12. 41.153.200 | BricLink II
+ 12. 41.153.201 | Comrex Access TOC #1 (WYCT)
+ 12. 41.153.202 | Comrex Access TOC #2 (WNRP)
+ 12. 41.153.203 | Comrex Access TOC #3 (WEBY/ESPN)
+ 12. 41.153.204 | Comrex Access Studio 5
+ 12. 41.153.205 | Internet Streaming
  12. 41.153.206 | Webrack/Podcast
                 </pre>
                 <i>future assignments</i>
             </div>
         </div>
         <div class="panel panel-info">
-            <div class="panel-heading"><?= Html::encode("WEBY Milton (Printer's Alley)"); ?></div>
+            <div class="panel-heading"><?= Html::encode('Cox in TOC (OfficeNet)'); ?></div>
             <div class="panel-body">
                 <address>
-                    <b>Modem s/n: </b>
+                    <b>Modem s/n: </b><u>Fiber</u>
                 </address>
                 <pre>
-255.255.255.248 | Netmask
- 66. 72.220.206 | Gateway
+255.255.255.252 | Netmask
+174. 79. 13. 81 | Gateway
+------------------------------------
+ 68.105. 28. 16 | Primary DSN
+ 68.105. 29. 16 | Secondary DSN
 ---------------------------------
- 66. 72.220.201 | Router ?
- 66. 72.220.202 |
- 66. 72.220.203 |
- 66. 72.220.204 |
- 66. 72.220.205 | NextGen WEBY-STL
-                </pre>
-            </div>
-        </div>
-    </div>
-
-    <div class="col-md-4">
-        <div class="panel panel-info">
-            <div class="panel-heading"><?= Html::encode('Cox in TOC (OFFICENET)'); ?></div>
-            <div class="panel-body">
-                <address>
-                    <b>Modem s/n: </b>
-                </address>
-                <pre>
-255.255.255.192 | Netmask
- 72.204.216.193 | Gateway
----------------------------------
- 72.204.216.221 | Sonicwall
-                </pre>
-            </div>
-        </div>
-        <div class="panel panel-info">
-            <div class="panel-heading"><?= Html::encode('Cox for STL (Plantation)'); ?></div>
-            <div class="panel-body">
-                <address>
-                    <b>Modem s/n: </b>215482974
-               </address>
-                <pre>
-255.255.255.192 | Netmask
- 72.204.216.193 | Gateway
----------------------------------
- 72.204.216.200 | NextGen WNRP-STL
-                </pre>
-            </div>
-        </div>
-        <div class="panel panel-info">
-            <div class="panel-heading"><?= Html::encode('DSL for STL (Plantation)'); ?></div>
-            <div class="panel-body">
-                <address>
-                    <b>Modem s/n: </b>
-                </address>
-                <pre>
-255.255.255.192 | Netmask
- 74.168.133. 50 | Gateway
----------------------------------
- 74.168.133. 50 | Netopia Router
- 74.168.133. 51 |
- 74.168.133. 52 |
- 74.168.133. 53 | Bric - 92.3 STL
- 74.168.133. 54 | NextGen WNRP-STL
+174. 79. 13. 82 | pfSense Firewall
                 </pre>
             </div>
         </div>
@@ -128,6 +77,26 @@ $this->params['breadcrumbs'][] = 'Public IP Addresses';
             </div>
         </div>
         <div class="panel panel-info">
+            <div class="panel-heading"><?= Html::encode('COX for STL (Jordan Street)'); ?></div>
+            <div class="panel-body">
+                <address>
+                    <b>Modem s/n: </b>
+                </address>
+                <pre>
+255.255.255.224 | Netmask
+ 98.191.245.225 | Gateway
+---------------------------------
+ 68.105. 28. 16 | Primary DNS
+ 68.105. 29. 16 | Secondary DNS
+=================================
+ 98.191.245.229 | LinkSys Router
+                </pre>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-4">
+        <div class="panel panel-info">
             <div class="panel-heading"><?= Html::encode('DSL for STL (Palafox)'); ?></div>
             <div class="panel-body">
                 <address>
@@ -147,7 +116,7 @@ $this->params['breadcrumbs'][] = 'Public IP Addresses';
             </div>
         </div>
         <div class="panel panel-info">
-            <div class="panel-heading"><?= Html::encode('UVerse for STL (Jordan)'); ?></div>
+            <div class="panel-heading"><?= Html::encode('UVerse for STL (Jordan Street)'); ?></div>
             <div class="panel-body">
                 <address>
                     <b>Modem s/n: </b>
@@ -161,6 +130,24 @@ $this->params['breadcrumbs'][] = 'Public IP Addresses';
 104. 10.109. 67 | 
 104. 10.109. 68 |
 104. 10.109. 69 |
+                </pre>
+            </div>
+        </div>
+        <div class="panel panel-info">
+            <div class="panel-heading"><?= Html::encode("AT&T for STL (Printers Alley)"); ?></div>
+            <div class="panel-body">
+                <address>
+                    <b>Modem s/n: </b>
+                </address>
+                <pre>
+255.255.255.248 | Netmask
+ 66. 72.220.206 | Gateway
+---------------------------------
+ 66. 72.220.201 | Router ?
+ 66. 72.220.202 |
+ 66. 72.220.203 |
+ 66. 72.220.204 |
+ 66. 72.220.205 | NextGen WEBY-STL
                 </pre>
             </div>
         </div>
